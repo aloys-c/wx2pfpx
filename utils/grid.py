4d1b1d2d97a866c4a6d4a6f91a5cf4b4b6e7d602
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-03-22 17:44:55 +0100

    Clean version

--- utils/grid.py
+++ utils/grid.py
@@ -12,13 +12,13 @@ with open("grid_list","w") as grid_file:
         for j in range(-180,180,1):
             n+=1
             line_out +="$"+str(n)+","+str(i)+","+str(j)+",1000\n"
-            json_item.extend([{"code":"$"+str(n),"lat":i,"lon":i}])
+            json_item.extend([{"code":"$"+str(n),"lat":i,"lon":j}])
 
     with open("grid","w") as json_file:
         json.dump(json_item,json_file)
     grid_file.write(line_out)
 
-if(1):
+if(0):
     n=0
     fig = plt.figure(figsize=(36, 18))
     m = Basemap(projection='cyl',lon_0=0.0, lat_0=0.0,width=36E6, height=18E6, resolution='c')
